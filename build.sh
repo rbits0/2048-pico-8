66aa7a5324d10de65c3480b4054c4abcd5ec6860
rbits0 <rbits0@gmail.com>
2024-09-12 10:05:01 +1000

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-pico8 main.p8 -export "2048.bin -i 13 -s 2"
-pico8 main.p8 -export "html/2048.js"
-pico8 main.p8 -export "2048.p8.png"
+mkdir build
+rm -r build/html
+cp -r html build
+pico8 main.p8 -export "build/2048.bin -i 13 -s 2"
+pico8 main.p8 -export "build/html/2048.js"
+pico8 main.p8 -export "build/2048.p8.png"
